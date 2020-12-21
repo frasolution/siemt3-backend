b159d526b72264a52e3a2b1a03fcd5edff7a26d0
Ugur Yilmaz <ugur.yilmaz@ditibma.onmicrosoft.com>
2020-12-21 17:17:54 +0100

    adds main class
    main class with Springboot annotation

---
+++ SIEMProject/src/main/java/com/mycompany/siemproject/SIEMApiApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.siemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIEMApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SIEMApiApp.class, args);
+    }
+
+}
