1ace8aae72f7bebc4ba3742f71308a98916ee395
Ugur Yilmaz <ugur.yilmaz@ditibma.onmicrosoft.com>
2020-12-21 17:18:46 +0100

    adds user + respository
    Adds user entity, but service and controller are still missing

---
+++ SIEMProject/src/main/java/com/mycompany/siemproject/model/User.java
@@ -0,0 +1,69 @@
+package com.mycompany.siemproject.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    
+    public boolean isPersisted() {
+        return null != id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ SIEMProject/src/main/java/com/mycompany/siemproject/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.mycompany.siemproject.repository;
+
+import com.mycompany.siemproject.model.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Override
+    List<User> findAll();
+
+    Optional<User> findByEmail(String email);
+    
+}
